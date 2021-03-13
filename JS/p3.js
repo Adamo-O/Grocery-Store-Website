d82d684206c6f4d96f0acda75bfb81f25c334b1e
Euphoric-Run6225 <59373798+Abhay520@users.noreply.github.com>
2021-03-12 21:59:39 -0500

    P3 add to cart button and part of the more description button

---
+++ JS/p3.js
@@ -0,0 +1,35 @@
+const documentTitle = document.title;
+
+function getData(){
+    let itemsArray = JSON.parse(sessionStorage.getItem('items'));
+    var itemName = document.title;
+    var price = document.getElementById("price").innerHTML;
+    var quantity = document.getElementById("quantity").value;
+    let itemData =[itemName,quantity,price];
+    itemsArray.push(itemData);
+    sessionStorage.setItem('items', JSON.stringify(itemsArray));
+}
+function addRows(){
+    var table = document.getElementById("table1");
+    let arr = JSON.parse(sessionStorage.getItem('items'));
+    for(let i =0;i<arr.length;i++){
+        var button = document.createElement("INPUT");
+        button.setAttribute("type","button");
+        button.setAttribute("value","x" );
+        var row = table.insertRow(i+1);
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML=arr[i][0];
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = arr[i][1];
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML = arr[i][2];
+        row.insertCell(3).appendChild(button);
+        button.onclick = function(){
+            document.getElementById("table1").deleteRow(i+1);
+            arr.splice(i, 1);
+            sessionStorage.setItem('items', JSON.stringify(arr));
+        };
+    }
+    console.log(arr[0]);
+}
+ 
