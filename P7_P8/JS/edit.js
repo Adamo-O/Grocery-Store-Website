98fc0a6f11022c4314fe6a8670dcd3494231bed1
Kunal Shah <37922021+K22shah@users.noreply.github.com>
2021-03-16 23:58:40 +0530

    added snack_edits

--- P7_P8/JS/edit.js
+++ P7_P8/JS/edit.js
@@ -1,22 +1,44 @@
-function edit() {
+function titleedit() {
+    
     var x = document.getElementById('title').value;
     document.getElementById('title').value = x;
-
-
-
-
+    alert("The product was successfully edited.")
+    return false;
+}
+function desedit() {
+    
     var des = document.getElementById('des').value;
     document.getElementById('des').value = des;
+    alert("The product was successfully edited.")
+    return false;
+}
 
+function priceedit() {
+    
     var price = document.getElementById('price').value;
     document.getElementById('price').value = price;
+    alert("The product was successfully edited.")
+    return false;
+}
 
+function stockedit() {
+    
     var stock = document.getElementById('stock').value;
     document.getElementById('stock').value = stock;
+    alert("The product was successfully edited.")
+    return false;
+}
 
+function dateedit() {
+    
     var date = document.getElementById('date').value;
     document.getElementById('date').value = date;
+    alert("The product was successfully edited.")
+    return false;
+}
 
+function imgedit() {
+    
     var img = document.getElementById('img');
     img.remove();
     var a = document.getElementById('button').files[0].name;
@@ -24,10 +46,10 @@ function edit() {
     i.src = a;
     var x = document.getElementById('q');
     x.append(i);
-
-alert("The product was successfully edited.")
-
+    alert("The product was successfully edited.")
     return false;
-
-
 }
+
+
+
+
