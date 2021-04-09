9d7c92a7e1cdb67cee3eac74305b8b4fc2ed8e37
Ghaith-Chrit <ghaithfinadnan@gmail.com>
2021-04-09 16:05:17 -0400

    edited the layout files

--- PHP/navbarAisle.php
+++ PHP/navbarAisle.php
@@ -1,15 +1,19 @@
-<?php
-echo '<section class="header p-3">
-<h1><a href="../index.html">La Meilleure Épicerie</a></h1>
+<section class="header p-3">
+  <h1><a href="../index.php">La Meilleure Épicerie</a></h1>
 </section>
 <section class="nav-bar">
-<ul class="nav-bar-content">
-  <li><a href="../index.html">Home</a></li>
-  <li><a href="../index.html#aisles">Aisles</a></li>
-  <li><a href="../index.html#offers">Offers</a></li>
-  <li><a href="../Login.html">Login</a></li>
-  <li><a href="../Sign Up.html">Sign Up</a></li>
-  <li><a href="../ShoppingCart.html" id="right">Shopping Cart</a></li>
-</ul>
-</section>';
-?>
+  <ul class="nav-bar-content">
+    <li><a href="../index.php">Home</a></li>
+    <li><a href="../index.php#aisles">Aisles</a></li>
+    <li><a href="../index.php#offers">Offers</a></li>
+    <?php
+    if (empty($_SESSION['logIn'])) {
+    ?>
+      <li><a href="../Login.php">Login</a></li>
+      <li><a href="../Sign Up.php">Sign Up</a></li>
+    <?php
+    }
+    ?>
+    <li><a href="../../HTML/ShoppingCart.html" id="right">Shopping Cart</a></li>
+  </ul>
+</section>
